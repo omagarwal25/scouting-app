479d412eeee923359b0ff5818133e0e136a394b4
omagarwal25 <lidagarwal@gmail.com>
2022-08-14 00:47:36 +0100

    fixed some ts issues. moved to solid because why not

--- apps/viewer/tailwind.config.js
+++ apps/viewer/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
