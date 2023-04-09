3269c7c9c1bdb3c0dd576826faee96673562925b
omagarwal25 <lidagarwal@gmail.com>
2023-04-09 02:51:38 +0100

    cleaner inputs

--- apps/client/components/input/PickerInput.tsx
+++ apps/client/components/input/PickerInput.tsx
@@ -1,19 +1,20 @@
+import { Picker } from '@react-native-picker/picker';
+import * as Haptics from 'expo-haptics';
 import {
   FieldValues,
   useController,
   UseControllerProps,
 } from 'react-hook-form';
-import { Picker } from '@react-native-picker/picker';
-import * as Haptics from 'expo-haptics';
-import { InputWrapper } from './InputWrapper';
+import { Pressable, Text, View } from 'react-native';
 import tw from '~/utils/tailwind';
+import { InputWrapper } from './InputWrapper';
 
 type Props<T extends FieldValues> = {
   control: UseControllerProps<T>;
   label: string;
   items: string[];
 };
-export const PickerInput = <T extends object>(props: Props<T>) => {
+const NativePickerInput = <T extends object>(props: Props<T>) => {
   const {
     field: { value, onChange },
   } = useController(props.control);
@@ -37,4 +38,42 @@ export const PickerInput = <T extends object>(props: Props<T>) => {
       </Picker>
     </InputWrapper>
   );
+};
+
+const MultiButton = <T extends object>(props: Props<T>) => {
+  const {
+    field: { value, onChange },
+  } = useController(props.control);
+
+  const onPick = async (e: unknown) => {
+    onChange(e);
+    await Haptics.selectionAsync();
+  };
+
+  return (
+    <InputWrapper label={`${props.label}: ${value}`}>
+      <View
+        style={tw`flex flex-row p-0.5 items-center justify-center mr-0 ml-auto`}
+      >
+        {props.items.map((e) => (
+          <Pressable
+            onPress={() => onPick(e)}
+            key={e}
+            style={tw`ml-2 border rounded p-3 flex items-center border-griffins-blue dark:border-pheonix-red ${
+              value !== e ? 'bg-griffins-blue dark:bg-pheonix-red' : ''
+            }`}
+          >
+            <Text style={tw`dark:text-white`}>{e}</Text>
+          </Pressable>
+        ))}
+      </View>
+    </InputWrapper>
+  );
+};
+
+export const PickerInput = <T extends object>(props: Props<T>) => {
+  if (props.items.length <= 5) {
+    return <MultiButton {...props} />;
+  }
+  return <NativePickerInput {...props} />;
 };
