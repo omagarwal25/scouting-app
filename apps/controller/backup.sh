28c92d245e89b5263c1909d7dfa6ef6d4e6bd2ee
omagarwal25 <lidagarwal@gmail.com>
2024-02-21 22:51:55 +0000

    schema change + backup sql db automatically

---
+++ apps/controller/backup.sh
@@ -0,0 +1,4 @@
+/opt/homebrew/bin/pg_dump scouting-app > /Users/oma/Documents/Coding/scouting-app/apps/controller/backups/$(date +%Y-%m-%d_%H-%M-%S).sql
+# TODO: Chnage this
+# TODO: also change the cron file
+# Run crontab cron when ready to go
